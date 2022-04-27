31af373cc965e3a7b88c373d207e7ee4f126577c
AlbertoRanno <albertoranno@hotmail.com>
2022-04-27 18:35:57 -0300

    probando imagenes en obj lit

--- clase20-pimientaYsal/src/app.js
+++ clase20-pimientaYsal/src/app.js
@@ -4,12 +4,8 @@ app.use(express.static("public"));
 
 //routes imports
 const rutasMain = require("./routes/main.js");
-const rutasProducts = require("./routes/products.js");
-const rutasUsers = require("./routes/users.js");
 
 app.use("/", rutasMain);
-app.use("/products", rutasProducts);
-app.use("/users", rutasUsers);
 
 //template engine
 app.set('view engine', 'ejs');
--- clase20-pimientaYsal/src/controllers/mainController.js
+++ clase20-pimientaYsal/src/controllers/mainController.js
@@ -1,7 +1,34 @@
 const mainController = {
   index: (req, res) => {
-    res.status(200).render("index");
+    res.status(200).render("index", { listadoDePlatos: platos });
+  },
+  detalleMenu: (req, res) => {
+    res.status(200).render("detalleMenu", { listadoDePlatos: platos });
   },
 };
 
 module.exports = mainController;
+
+const platos = [
+  {
+    nombre: "Carpaccio fresco",
+    descripcion: "Entrada Carpaccio de salmón con cítricos",
+    precio: 65.5,
+    imagen: "/images/Carpaccio-de-salmon.jpg",
+  },
+  {
+    nombre: "Risotto de berenjena",
+    descripcion: "Risotto de berenjena y queso de cabra",
+    precio: 47.0,
+  },
+  {
+    nombre: "Mousse de arroz",
+    descripcion: "Mousse de arroz con leche y aroma de azahar",
+    precio: 27.5,
+  },
+  {
+    nombre: "Espárragos blancos",
+    descripcion: "Espárragos blancos con vinagreta de verduras y jamón ibérico",
+    precio: 37.5,
+  },
+];
